d9a635fc5aeb71c73d389b78efb59222aefa1ad8
mohammadamin45 <mohammad.amin2342@gmail.com>
2024-06-03 14:30:38 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const counterEl = document.getElementById('counter');
+let count = 0;
+
+function updateCounter() {
+  counterEl.textContent = count;
+}
+
+updateCounter();
+
+const decrementBtn = document.getElementById('decrement');
+decrementBtn.addEventListener('click', () => {
+  if (count > 0) {
+    count--;
+    updateCounter();
+  }
+});
+
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', () => {
+  count = 0;
+  updateCounter();
+});
+
+const incrementBtn = document.getElementById('increment');
+incrementBtn.addEventListener('click', () => {
+  count++;
+  updateCounter();
+});
